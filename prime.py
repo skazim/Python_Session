4d8a80e57e157383cb86944827e33cbb94a4aedd
skazim <kazimmuhammad10@gmail.com>
2018-09-18 13:00:17 +0500

    More about Pandas DataFrames

--- prime.py
+++ prime.py
@@ -1,10 +1,10 @@
 num = 8
 
 if num > 1:
-  for i in range(2,num):
-    if (num % i) == 0:
-      print(num, "i not a prime number")
-      print(i,"times", num//i, "is", num)
-      break
-  else:
-    print(num, "is a prime number")
+    for i in range(2,num):
+        if (num % i) == 0:
+            print(num, "i not a prime number")
+            print(i,"times", num//i, "is", num)
+            break
+    else:
+        print(num, "is a prime number")
